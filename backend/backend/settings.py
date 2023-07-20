2ca16e8fcd089508dc6895d914eda61b2bdfc225
nikitaloskutov18 <nikitaloskutov18@gmail.com>
2023-07-20 17:53:34 +0300

    Deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -81,18 +81,28 @@ DATABASES = {
 
 # Password validation
 
+validators = {
+    "django.contrib.authpassword_validation.UserAttributeSimilarityValidator"
+}
+
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.authpassword_validation.UserAttributeSimilarityValidator",
+        "NAME": validators,
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "NAME": (
+            "django.contrib.auth.password_validation.MinimumLengthValidator"
+        ),
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        "NAME": (
+            "django.contrib.auth.password_validation.CommonPasswordValidator"
+        ),
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        "NAME": (
+            "django.contrib.auth.password_validation.NumericPasswordValidator"
+        ),
     },
 ]
 
