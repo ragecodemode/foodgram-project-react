50af39d3774cfbd6dc8c922ae094b63844a7edd1
nikitaloskutov18 <nikitaloskutov18@gmail.com>
2023-07-26 12:46:39 +0300

    Deploy 5

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -81,8 +81,7 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.authpassword_validation."
-                "UserAttributeSimilarityValidator",
+        "NAME": "django.contrib.authpassword_validation.UserAttributeSimilarityValidator",
     },
     {
         "NAME": (
