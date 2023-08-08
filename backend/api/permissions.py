43f1f5e786b5b33fb76183a0f259eda6f958afa6
nikitaloskutov18 <nikitaloskutov18@gmail.com>
2023-08-08 20:44:37 +0300

    Deploy recipe permissions

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -8,14 +8,5 @@ class IsAuthenticatedOrReadOnly(IsAuthenticatedOrReadOnly):
     Создания, изменения, удаления только автору и администратору.
     """
 
-    def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_staff
-        )
-
     def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or obj.author == request.user
-        )
+        return request.method in permissions.SAFE_METHODS or request.user == obj.author
