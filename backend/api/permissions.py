54273b3251ddac691f33461442711e7d4f8a0ffb
nikitaloskutov18 <nikitaloskutov18@gmail.com>
2023-08-08 21:04:40 +0300

    Deploy fix recipes permissions

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,5 +1,4 @@
-from rest_framework import permissions
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import SAFE_METHODS, IsAuthenticatedOrReadOnly
 
 
 class IsAuthenticatedOrReadOnly(IsAuthenticatedOrReadOnly):
@@ -8,14 +7,20 @@ class IsAuthenticatedOrReadOnly(IsAuthenticatedOrReadOnly):
     Создания, изменения, удаления только автору и администратору.
     """
 
+    def has_permission(self, request, view):
+        return (
+            request.method in SAFE_METHODS
+            or request.user.is_authenticated
+        )
+
     def has_object_permission(self, request, view, obj):
-        """
-        Проверяет, имеет ли пользователь доступ к объекту.
-        """
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        if request.user.is_staff:
-            return True
-
-        return request.user == obj.author
+        return (
+            request.method in SAFE_METHODS
+            or (
+                request.user.is_authenticated
+                and (
+                    obj.author == request.user
+                    or request.user.is_superuser
+                )
+            )
+        )
