2522aba441765148ed51130fd97137b0cdcadf6e
nikitaloskutov18 <nikitaloskutov18@gmail.com>
2023-08-08 20:54:59 +0300

    Deploy fix recipe permissions

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -9,4 +9,13 @@ class IsAuthenticatedOrReadOnly(IsAuthenticatedOrReadOnly):
     """
 
     def has_object_permission(self, request, view, obj):
-        return request.method in permissions.SAFE_METHODS or request.user == obj.author
+        """
+        Проверяет, имеет ли пользователь доступ к объекту.
+        """
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        if request.user.is_staff:
+            return True
+
+        return request.user == obj.author
