cc3d92bc7ec58f378d6b3bcc97384831578e4b43
nikitaloskutov18 <nikitaloskutov18@gmail.com>
2023-08-09 14:59:07 +0300

    Permissions

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,11 +1,14 @@
-from rest_framework.permissions import SAFE_METHODS, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import BasePermission
 
 
-class IsAuthorOrReadOnly(IsAuthenticatedOrReadOnly):
+class IsAuthenticatedOrReadOnly(BasePermission):
     """
     Чтения любым пользователям.
     Создания, изменения, удаления только автору и администратору.
     """
 
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
-        return request.method in SAFE_METHODS or request.user == obj.author
+        return True
