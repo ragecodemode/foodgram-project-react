3a8ed9f26b2b99a250b34ae42c4ad40a58dac7d0
nikitaloskutov18 <nikitaloskutov18@gmail.com>
2023-08-18 10:52:15 +0300

    Deploy rc

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -178,10 +178,10 @@ class RecipeRetrieveUpdate(serializers.ModelSerializer):
 
     @atomic
     def create(self, validated_data):
-        request = self.context.get('request')
+        # request = self.context.get('request')
         tags = validated_data.pop('tags')
         ingredients = validated_data.pop('ingredients')
-        recipe = Recipe.objects.create(author=request.user, **validated_data)
+        recipe = Recipe.objects.create(**validated_data)
 
         for tag in tags:
             recipe.tags.add(tag)
@@ -192,7 +192,7 @@ class RecipeRetrieveUpdate(serializers.ModelSerializer):
                 amount=amount,
                 ingredient=ingredient['id'],
                 recipe=recipe
-            )
+            ).save()
         return recipe
 
     @atomic
