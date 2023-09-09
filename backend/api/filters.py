eec4479a05d83a274b07a6a35ef75753e4018fa4
nikitaloskutov18 <nikitaloskutov18@gmail.com>
2023-09-09 18:46:10 +0300

    Deploy

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,16 +1,17 @@
 from rest_framework.filters import SearchFilter
+from recipes.models import Ingredient
 
 
 class IngredientSearch(SearchFilter):
     """Поиск по вхождению в начало имени ингредиента."""
 
-    def filter_queryset(self, request, queryset, view):
-        search_query = request.query_params.get('name')
-        starts_with_queryset = queryset.filter(name__istartswith=search_query)
-        contains_queryset = queryset.filter(name__icontains=search_query)
+    # def filter_queryset(self, request, queryset, view):
+    #     search_query = request.query_params.get('name')
+    #     starts_with_queryset = queryset.filter(name__istartswith=search_query)
+    #     contains_queryset = queryset.filter(name__icontains=search_query)
 
-        queryset = starts_with_queryset | contains_queryset
-        return queryset
+    #     queryset = starts_with_queryset | contains_queryset
+    #     return queryset
 
     # def get_search_fileds(self, view, request):
     #     if request.query_params.get('name'):
@@ -21,6 +22,6 @@ class IngredientSearch(SearchFilter):
 
     # search_param = 'name'
 
-    # class Meta:
-    #     model = Ingredient
-    #     fields = ('name',)
+    class Meta:
+        model = Ingredient
+        fields = ('name',)
